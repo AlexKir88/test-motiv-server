b0252f2610765d72eca920968b6be8efffd0e347
Alex_K <as.kirsanov@yandex.ru>
2023-05-10 06:26:54 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const proxy = require('express-http-proxy');
-//попробовать передать ещлут на клиент через что-то (что? хедер не видит, осталось боди)
-let token; //пока так
+
+let token;
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 
 app.use('/', proxy('https://test-dev.motiw.ru',{
     userResHeaderDecorator: (hdrs) => {
-        // let token = hdrs["set-cookie"];
-        // if(token) hdrs.token = token;
         let newToken = hdrs["set-cookie"];
         if(newToken) {
             token = newToken;
@@ -19,8 +17,6 @@ app.use('/', proxy('https://test-dev.motiw.ru',{
         return hdrs;
     },
     proxyReqOptDecorator: (reqOpts) => {
-        // reqOpts.headers.cookie = ['PHPSESSID=eskpn75cu9kfpe4hpdvjhu90lv; path=/'];
-        // let token = reqOpts.headers.token;
         if(token) reqOpts.headers.cookie = token;
         return reqOpts;
     },
